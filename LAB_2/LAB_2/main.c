3b6becc6d6d58711b39b7b0be35069308f1ed4c8
AnjeyNov <AnjeyNov@gmail.com>
2019-03-13 15:33:47 +0300

    remake LAB_2

--- LAB_2/LAB_2/main.c
+++ LAB_2/LAB_2/main.c
@@ -12,30 +12,23 @@
 
 short A[64], B[64], C[64];
 
-void andC(void);												// логическое » дл€ двух матриц на €зыке —и
-
-void andASM(void);												// логическое » дл€ двух матриц на €зыке ассемблер
-																// без использовани€ набора команд MMX
-
-void andMMX(void);												// логическое » дл€ дву матриц на €зыке ассемблер
-																// с использованием набора команд MMX
-
-void outMatrix(short *C);											// вывод результата
+void andC(void);												// AND for two matrix on C
+void andASM(void);												// AND for two matrix on .asm
+void andMMX(void);												// AND for two matrix on .asm with MMX
+															
+void outMatrix(short *C);										// out result
 
 int main(int argc, char *argv[]) {
 	
-	// инициализаци€ двух матриц
 	for (int i = 0; i < 64; i++) {
 		A[i] = (i/3) * 2;
 		B[i] = (i*3) + 2;
 	}
 
-	// выполнение
 	andC();
 	andASM();
 	andMMX();
 
-	// ожидани€ нажати€ дл€ последующего выхода
 	_getch();
 	return 0;
 }
@@ -44,11 +37,9 @@ void andC(void)
 {
 	double t;
 	LARGE_INTEGER timerFrequency, timerStart, timerStop;
-	//short C[8][8];
 	QueryPerformanceFrequency(&timerFrequency);					
 	QueryPerformanceCounter(&timerStart);
 
-	// выполн€ем логическое » дл€ соответствующих элементов и заносим резальтат в матрицу —
 	for (double k = 0; k < 10000000; k++) {
 		for (int i = 0; i < 64; i++) {
 			C[i] = A[i] & B[i];
@@ -57,11 +48,9 @@ void andC(void)
 
 	QueryPerformanceCounter(&timerStop);
 
-	// расчет и вывод затраченного времени
 	t = (double)(timerStop.QuadPart - timerStart.QuadPart) / (double)timerFrequency.QuadPart;
 	printf("\nTime C is %lf seconds.\n", t);
 	
-	// вывод результата
 	outMatrix(C);					
 	return;
 }
@@ -74,31 +63,26 @@ void andASM(void)
 	QueryPerformanceCounter(&timerStart);
 
 	_asm {
-		pusha									// сохранить в стэк все регистры
+		pusha									// save all reg in stack
 
 		xor ECX,ECX								// EXC = 0
 		xor EAX, EAX							// EAX = 0
 
 		mov ECX, 10000000						// ECX = 10000000
 		loop0:									// for(; ECX > 0; ECX--){
-			push ECX							//		загрузить регистр ECX в стек	
-			mov ECX, 08h						//		ECX = 8
+			push ECX							//		save ECX in stack	
+			mov ECX, 64							//		ECX = 8
 			xor EDI, EDI
 			loop1:								//		for(; ECX > 0; ECX--){
-				push ECX						//			загрузать регистр ECX в стек
-				mov ECX, 08h					//			ECX = 8
-				loop2:							//			for(; ECX > 0; ECX--){
-					mov AX, A[EDI]				//				AX = A[EDI]
-					and AX, B[EDI]				//				AX = AX & B[EDI] 
-					mov C[EDI], AX				//				C[EDI] = AX
-					inc EDI						//				EDI++
-					inc EDI						//				EDI++
-				loop loop2						//			}
-					pop ECX						//			выгрузить ECX из стека
+				mov AX, A[EDI]					//				AX = A[EDI]
+				and AX, B[EDI]					//				AX = AX & B[EDI] 
+				mov C[EDI], AX					//				C[EDI] = AX
+				inc EDI							//				EDI++
+				inc EDI							//				EDI++
 			loop loop1							//		}
-		pop ECX									// выгрузить ECX из стека
+		pop ECX									// get ECX form stack
 		loop loop0								// }
-		popa									// выгрузить все регистры из стека
+		popa									// get all reg form stack
 	}
 
 	QueryPerformanceCounter(&timerStop);
@@ -127,18 +111,13 @@ void andMMX(void)
 		mov ECX, 10000000						// ECX = 10000000
 		loop0 :									// for(; ECX > 0; ECX--){
 			push ECX							//		загрузить ECX в стек
-			mov ECX, 08h						//		ECX = 8
+			mov ECX, 16 						//		ECX = 8
 			xor EDI, EDI						//			EDI = 0
-			loop1 :								//		for(; ECX > 0; ECX--){
-				push ECX						//			загрузить ECX в стек
-				mov ECX, 02h					//			ECX = 8
-				loop2 :							//			for(; ECX > 0; ECX--){
-					movq MM0, A[EDI]			//				MM0 = { A[EDI], A[EDI+2], A[EDI+4], A[EDI+6] }
-					pand MM0, B[EDI]			//				MM0 & { B[EDI], B[EDI+2], B[EDI+4], B[EDI+6] }
-					movq C[EDI], MM0			//				{ C[EDI], C[EDI+2], C[EDI+4], C[EDI+6] } = MM0
-					add EDI, 08h				//				EDI +=8
-				loop loop2						//			}
-				pop ECX							//			выгрузить ECX из стека
+			loop1:								//		for(; ECX > 0; ECX--){
+				movq MM0, A[EDI]				//				MM0 = { A[EDI], A[EDI+2], A[EDI+4], A[EDI+6] }
+				pand MM0, B[EDI]				//				MM0 & { B[EDI], B[EDI+2], B[EDI+4], B[EDI+6] }
+				movq C[EDI], MM0				//				{ C[EDI], C[EDI+2], C[EDI+4], C[EDI+6] } = MM0
+				add EDI, 08h					//				EDI +=8
 			loop loop1							//		}
 			pop ECX								//		выгрузить ECX из стека
 		loop loop0								// }
